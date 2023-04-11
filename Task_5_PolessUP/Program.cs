c017d68c672fa3496ffbe58f40dd7b689a089fa7
CyberSanyok <sashad92175@gmail.com>
2023-04-11 19:11:56 +0300

    Добавьте файлы проекта.

---
+++ Task_5_PolessUP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] ints = { 2, 3, 1 };
+        int max = ints[0];
+        for (int i = 1; i < ints.Length; i++) 
+        {
+            if (ints[i]>max)max= ints[i];
+        }
+        Console.WriteLine(max);
+    }
+}
